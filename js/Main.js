1a82d66c1fcc0d497dfde139364730ed32c8f059
Ricardo Almeida de Souza <ricardosouza@ice.ufjf.br>
2021-03-20 21:31:42 -0300

    Revert "Adiciona corredor e inimigo em posição aleatória"
    This reverts commit 832efd5876b8086e9f7c378acd83e8056702a6e4.

--- js/Main.js
+++ js/Main.js
@@ -24,13 +24,13 @@ const mapa1 = new Mapa(10, 14, 32);
 mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
-const pc = new Sprite({x: 50, y: 144, vx: 10});
+const pc = new Sprite({x: 50, y: 150, vx: 10});
 const en1 = new Sprite({x: 160, vx: -10, color:"red"});
 
 cena1.adicionar(pc);
-// cena1.adicionar(en1);
-cena1.adicionar(new Sprite({x: canvas.width * (Math.random()), y: canvas.height * Math.random(), vy: 10 * Math.random(), vx: 10 * Math.random(), color:"red"}));
-cena1.adicionar(new Sprite({x: 200, y: 140, vx: -10, color:"red"}));
+cena1.adicionar(en1);
+cena1.adicionar(new Sprite({x: 115, y: 70, vy: 10, color:"red"}));
+cena1.adicionar(new Sprite({x: 115, y: 160, vy: -10, color:"red"}));
 
 cena1.iniciar();
 
